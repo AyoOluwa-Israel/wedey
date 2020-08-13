cfc84843d03a878d666d3db25bdca4d19c337652
AyoOluwa-Izzy <67590380+AyoOluwa-Izzy@users.noreply.github.com>
2020-08-13 11:39:54 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener("scroll", function () {
+    let menuArea = document.getElementById('menu-area');
+    if (window.pageYOffset > 0) {
+      menuArea.classList.add("cus-nav");
+    }else {
+      menuArea.classList.remove("cus-nav");
+    }
+});
+
+
+const topnav = document.getElementById("topnav");
+const topnavToggle = topnav.querySelector(".menu-toggle");
+
+function openMobileNavbar() {
+  topnav.classList.add("opened");
+  topnavToggle.setAttribute("aria-label", "Close navigation menu.");
+}
+
+function closeMobileNavbar() {
+  topnav.classList.remove("opened");
+  topnavToggle.setAttribute("aria-label", "Open navigation menu.");
+}
+
+topnavToggle.addEventListener("click", () => {
+  if (topnav.classList.contains("opened")) {
+    closeMobileNavbar();
+  } else {
+    openMobileNavbar();
+  }
+});
+
+const topnavMenu = topnav.querySelector(".nav-menu");
+const topnavLinks = topnav.querySelector(".nav-links");
+
+topnavLinks.addEventListener("click", (clickEvent) => {
+  clickEvent.stopPropagation();
+});
+
+topnavMenu.addEventListener("click", closeMobileNavbar);
